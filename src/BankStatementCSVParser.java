57efcaa59d4e6c06ac612a9e813fb6e527ac97ba
Jamal Hassan <jayhamo22@gmail.com>
2022-12-06 10:43:13 +0300

    adds....

--- src/BankStatementCSVParser.java
+++ src/BankStatementCSVParser.java
@@ -1,2 +1,7 @@
+import java.time.format.DateTimeFormatter;
+
 public class BankStatementCSVParser {
+    private static final DateTimeFormatter DATE_PATTERN
+            = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
 }
