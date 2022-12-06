11071fe75456b7fb80dfc1802bfce0095bfe4310
Jamal Hassan <jayhamo22@gmail.com>
2022-12-06 11:38:47 +0300

    adds....

--- src/BankTransaction.java
+++ src/BankTransaction.java
@@ -33,4 +33,15 @@ public class BankTransaction {
                 ", description='" + description + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return
+                false;
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
 }
