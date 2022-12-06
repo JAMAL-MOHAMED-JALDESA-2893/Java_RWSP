892f95b773b80ee4c3a62be8946956033c1c0de5
Jamal Hassan <jayhamo22@gmail.com>
2022-12-06 10:48:36 +0300

    adds....

--- src/BankStatementCSVParser.java
+++ src/BankStatementCSVParser.java
@@ -1,8 +1,13 @@
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class BankStatementCSVParser {
     private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     private BankTransaction parseFromCSV(final String line) {
         final String[] columns = line.split(",");
+        final LocalDate date = LocalDate.parse(columns[0],
+                DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
 
-}
+
+    }
